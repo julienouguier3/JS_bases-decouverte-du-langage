c670f9be37c6d96e1ae66dbeace82c935656c8c2
julienouguier3 <nouguier.j3@gmail.com>
2023-01-11 16:53:36 +0100

    exo-2_test_en_cours

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,33 @@
 // you can write js here
-console.log('exo-2');
+
+let testing = prompt("What day is it?");
+const isTesting = true;
+
+if (day=1 && hour>9 || day>=5 && hour>17) {
+    console.log(isTesting);
+}
+else {
+    console.log(!isTesting);
+}
+    
+
+const myDate = new Date();
+console.log(myDate);
+
+const weekday = "still waiting";
+const weekend = "Nice WE";
+
+let day = myDate.getDay();
+console.log(day);
+
+let hour = myDate.getHours();
+console.log(hour);
+
+
+    if (day=1 && hour>9 || day>=5 && hour>17) {
+        console.log(weekday);
+    }
+    else {
+        console.log(weekend);
+    }
+
