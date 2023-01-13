e7c9471dfbabfd840d1852e39fef33aa774b0d78
julienouguier3 <nouguier.j3@gmail.com>
2023-01-13 16:51:39 +0100

    exo-5 terminé

--- exo-4/script.js
+++ exo-4/script.js
@@ -29,27 +29,23 @@ var secretMessage = [
 
 //enlever le dernier élément du tableau
 secretMessage.pop();
-console.log(secretMessage);
 
 //ajouter les mots 'to' and 'program'
 secretMessage.push("to", "program");
-console.log(secretMessage);
 
 //Changer le mot 'easily' par 'right'
-secretMessage[6] = "right";
-console.log(secretMessage);
+secretMessage[6] = "right"; //secretMessage=[secretMessage.indexOf("easily"])='right';
 
 //supprimer le premier élément du tableau
 secretMessage.shift();
-console.log(secretMessage);
 
 //ajouter la chaine "Programming" en début
 secretMessage.unshift("Programming");
-console.log(secretMessage);
 
 // remplacer la série de cinq mots : 'get', 'right', 'the', 'first', 'time' par un seul mot : 'know'
-replace = secretMessage.splice(5, [5], "know");//splice (nbdébut, nbrsuppr, éléments)
-console.log(secretMessage);
-
+replace = secretMessage.splice(5, [5], "know"); //splice (nbdébut, nbrsuppr, éléments)
+//secretMessage.splice(5, [5], "know");
 //méthode `.join` pour afficher le message secret
 console.log(secretMessage.join(" "));
+
+console.log(secretMessage);
--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,21 @@
 // you can write js here
 
-console.log('exo-5');
+console.log("exo-5");
+var input = prompt("Entrez un mot ou une phrase");
+input = input.toLowerCase();
+
+const vowels = ["a", "e", "i", "o", "u", "y"];
+const resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    console.log(input[i]);
+    (vowels.indexOf(input[i]) > -1) ? resultArray.push(input[i]) : null;
+    //console.log(vowels.indexOf(input[i]));
+}
+
+console.log(resultArray.join("").toUpperCase());
+
+
+//print et appel
+// console.log(input);
+//console.log(resultArray.includes(input,vowels));
